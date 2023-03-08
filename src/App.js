b8462c8aff3cbb22c66075afd72b60a18b8cce38
chriskimty <christaeyoung.kim@gmail.com>
2023-03-08 13:56:22 -0500

    refactored code and added empty shell to three main pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import { Link, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
-import EyesLandingPage from './components/EyesLandingPage';
-import LipsLandingPage from './components/LipsLandingPage';
-import PureColor from './components/PureColor';
-import PureColorEnvy from './components/PureColorEnvy';
-import SmokeAndBrighten from './components/SmokeAndBrighten';
+import CategoryPage from './components/CategoryPage';
+import ProductPage from './components/ProductPage';
+
 import './styles/styles.scss';
 
 const App = () => {
   return (
     <div className="App">
-      <header>
-        <Link to="/">
-          <h1>Chrōma</h1>
-        </Link>
-      </header>
-
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/eyes" element={<EyesLandingPage />} />
-        <Route path="/lips" element={<LipsLandingPage />} />
-        <Route path="/eyes/smoke-and-brighten" element={<SmokeAndBrighten />}/>
-        <Route path="/lips/pure-color" element={<PureColor />} />
-        <Route path="/lips/pure-color-envy" element={<PureColorEnvy />} />
+        {/* Thinking that both the category and product pages each needs a CUSTOM route (i.e. custom param)  */}
+        <Route path="/category-page" element={<CategoryPage />} />
+        <Route path="/category-page/product-page" element={<ProductPage />}/>
       </Routes>
     </div>
   );
---
+++ src/components/CategoryPage.js
@@ -0,0 +1,49 @@
+// import { useState } from 'react';
+
+import ProductPage from "./ProductPage";
+
+// CategoryPage pseudocode (one page for both eyes and lips)
+// - In the productCard:
+//     - image 
+//     - brand
+//     - name of product 
+//     - how many shades
+//     - swatch img
+// - when user clicks on a productCard, it calls API (take the name of the product)
+// (onClick)
+
+const CategoryPage = () => {
+    // For now, created the state and basic function here, which can be called when the products are clicked! 
+    // const [isClicked, setIsClicked] = useState(false);
+    // const handleProductsClick = () => {
+    //   setIsClicked(true);
+    // };
+
+    return (
+        <section className="categoryPage">
+            {/* empty {}jsx curlies to indicate the data that needs to go in there according to the API call. In our case, it will either say LIPS or EYES */}
+            <h2>{ }</h2>
+            {/* I'm thinking of an empty ul in here so that whatever products we have listed in our database can be displayed here as an li */}
+            {/* We'll need to create some kind of forEach or map function so that we can get it displayed accordingly!! */}
+            <ul className="productSelections">
+                {/* commenting out for now but each will look something like this that we'll map out */}
+                {/* <li onClick={handleProductsClick}>
+                        IMAGE:
+                        <img src={ } alt="" /> 
+                        BRAND:
+                        <p>{ }</p>
+                        NAME OF PRODUCT:
+                        <p>{ }</p>
+                        HOW MANY SHADES:
+                        <p>{ }shades</p>
+                        SWATCH IMAGE: 
+                        <img src={ } alt="" />
+                </li> */}
+            </ul>
+
+            {/* {isClicked && <ProductPage/>} */}
+        </section>
+    )
+}
+
+export default CategoryPage;
