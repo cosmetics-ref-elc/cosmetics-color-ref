63012c285e68926294894dc0469dc8e628d2c96b
Neha Bhole <nehasbhole@gmail.com>
2023-03-15 16:50:43 -0400

    db updated with img links, compnents updated and styling added but needs to be fixed

--- src/components/CategoryCard.js
+++ src/components/CategoryCard.js
@@ -1,24 +1,29 @@
 import { Link } from 'react-router-dom';
-import placeholderImg from '../assets/LandingPage/Lips.jpg';
+// import placeholderImg from '../assets/LandingPage/Lips.jpg';
+// import images from '../assets/LandingPage'
 import { useGray } from "../context/ToggleGrayscale";
+
+
 const CategoryCard = ({ categories }) => {
 
-  const { isActive } = useGray();
+    const { isActive } = useGray();
 
-  const category_cards = categories.map((cat) => (
-    <div key={cat._id} id={cat.name} className="catCards">
-      <Link to={"/category/" + cat.name.toLowerCase()} state={cat}>
-        {/* placeholder images for now */}
-        <img
-          src={placeholderImg}
-          alt={cat.name}
-          className="catCardsImg"
-          style={{ filter: isActive ? "grayscale(100%)" : "none" }}
-        />
-        <h3>{cat.name}</h3>
-      </Link>
-    </div>
-  ));
+    const category_cards = categories.map((cat) => (
+        <div key={cat._id} id={cat.name} className="catCards">
+            <Link to={"/category/" + cat.name.toLowerCase()} state={cat}>
+                {/* placeholder images for now */}
+                <img
+                    // src={placeholderImg}
+                    // src={images[cat.name.toLowerCase()]}
+                    src={cat.img}
+                    alt={cat.name}
+                    className="catCardsImg"
+                    style={{ filter: isActive ? "grayscale(100%)" : "none" }}
+                />
+                <h3>{cat.name}</h3>
+            </Link>
+        </div>
+    ));
 
     return (
         <div className="categoryCard">
