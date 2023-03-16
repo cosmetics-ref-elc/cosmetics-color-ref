1deeb59818222d9647fbfdd4dc5387144c2983c3
chriskimty <christaeyoung.kim@gmail.com>
2023-03-15 22:19:52 -0400

    made small change

--- src/components/CategoryCard.js
+++ src/components/CategoryCard.js
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useGray } from "../context/ToggleGrayscale";
 const CategoryCard = ({ categories }) => {
-
   const { isActive } = useGray();
 
   const category_cards = categories.map((cat) => (
