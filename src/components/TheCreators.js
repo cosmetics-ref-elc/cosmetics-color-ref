552f9ec266de31cdaa59bd1d8a139de8f51da838
chriskimty <christaeyoung.kim@gmail.com>
2023-03-17 18:17:14 -0400

    added shell for the creators page

--- src/components/TheCreators.js
+++ src/components/TheCreators.js
@@ -1,7 +1,203 @@
+import caiyi from "../assets/TheCreators/caiyi.JPG";
+import gitHubIcon from "../assets/TheCreators/GitHub.png";
+import linkedInIcon from "../assets/TheCreators/LinkedIn.png";
+import portfolioIcon from "../assets/TheCreators/Portfolio.png";
 const TheCreators = () => {
   return (
     <section className="theCreators">
-          <h2>The Creators</h2>
+      <h2>Meet the Creators</h2>
+      <ul className="creatorsGrid">
+        <li className="creatorCard">
+          <div className="imgContainer">
+            <img src={caiyi} alt="" />
+          </div>
+          <div className="textContainer">
+            <h3>Caiyi Chen</h3>
+            <p>Software Developer</p>
+            <ul className="socialsList">
+              <li className="socials">
+                <a href="https://github.com/Sputnikccy">
+                  <img
+                    className="icon"
+                    src={gitHubIcon}
+                    alt="github icon"
+                  ></img>
+                </a>
+              </li>
+              <li className="socials">
+                <a href="https://github.com/Sputnikccy">
+                  <img
+                    className="icon"
+                    src={linkedInIcon}
+                    alt="linkedIn icon"
+                  ></img>
+                </a>
+              </li>
+              <li className="socials">
+                <a href="https://github.com/Sputnikccy">
+                  <img
+                    className="icon"
+                    src={portfolioIcon}
+                    alt="portfolio icon"
+                  ></img>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li className="creatorCard">
+          <div className="imgContainer">
+            <img src={caiyi} alt="" />
+          </div>
+          <div className="textContainer">
+            <h3>Caiyi Chen</h3>
+            <p>Software Developer</p>
+            <ul className="socialsList">
+              <li className="socials">
+                <a href="https://github.com/Sputnikccy">
+                  <img
+                    className="icon"
+                    src={gitHubIcon}
+                    alt="github icon"
+                  ></img>
+                </a>
+              </li>
+              <li className="socials">
+                <a href="https://github.com/Sputnikccy">
+                  <img
+                    className="icon"
+                    src={linkedInIcon}
+                    alt="linkedIn icon"
+                  ></img>
+                </a>
+              </li>
+              <li className="socials">
+                <a href="https://github.com/Sputnikccy">
+                  <img
+                    className="icon"
+                    src={portfolioIcon}
+                    alt="portfolio icon"
+                  ></img>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li className="creatorCard">
+          <div className="imgContainer">
+            <img src={caiyi} alt="" />
+          </div>
+          <div className="textContainer">
+            <h3>Caiyi Chen</h3>
+            <p>Software Developer</p>
+            <ul className="socialsList">
+              <li className="socials">
+                <a href="https://github.com/Sputnikccy">
+                  <img
+                    className="icon"
+                    src={gitHubIcon}
+                    alt="github icon"
+                  ></img>
+                </a>
+              </li>
+              <li className="socials">
+                <a href="https://github.com/Sputnikccy">
+                  <img
+                    className="icon"
+                    src={linkedInIcon}
+                    alt="linkedIn icon"
+                  ></img>
+                </a>
+              </li>
+              <li className="socials">
+                <a href="https://github.com/Sputnikccy">
+                  <img
+                    className="icon"
+                    src={portfolioIcon}
+                    alt="portfolio icon"
+                  ></img>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li className="creatorCard">
+          <div className="imgContainer">
+            <img src={caiyi} alt="" />
+          </div>
+          <div className="textContainer">
+            <h3>Caiyi Chen</h3>
+            <p>Software Developer</p>
+            <ul className="socialsList">
+              <li className="socials">
+                <a href="https://github.com/Sputnikccy">
+                  <img
+                    className="icon"
+                    src={gitHubIcon}
+                    alt="github icon"
+                  ></img>
+                </a>
+              </li>
+              <li className="socials">
+                <a href="https://github.com/Sputnikccy">
+                  <img
+                    className="icon"
+                    src={linkedInIcon}
+                    alt="linkedIn icon"
+                  ></img>
+                </a>
+              </li>
+              <li className="socials">
+                <a href="https://github.com/Sputnikccy">
+                  <img
+                    className="icon"
+                    src={portfolioIcon}
+                    alt="portfolio icon"
+                  ></img>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li className="creatorCard">
+          <div className="imgContainer">
+            <img src={caiyi} alt="" />
+          </div>
+          <div className="textContainer">
+            <h3>Caiyi Chen</h3>
+            <p>Software Developer</p>
+            <ul className="socialsList">
+              <li className="socials">
+                <a href="https://github.com/Sputnikccy">
+                  <img
+                    className="icon"
+                    src={gitHubIcon}
+                    alt="github icon"
+                  ></img>
+                </a>
+              </li>
+              <li className="socials">
+                <a href="https://github.com/Sputnikccy">
+                  <img
+                    className="icon"
+                    src={linkedInIcon}
+                    alt="linkedIn icon"
+                  ></img>
+                </a>
+              </li>
+              <li className="socials">
+                <a href="https://github.com/Sputnikccy">
+                  <img
+                    className="icon"
+                    src={portfolioIcon}
+                    alt="portfolio icon"
+                  ></img>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </li>
+      </ul>
     </section>
   );
 };
